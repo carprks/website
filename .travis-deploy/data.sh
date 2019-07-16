fc34da3279cfdc6915aa62281329ab771b7a1079
Keloran <keloran@nordicarts.net>
2019-07-16 20:47:37 +0100

    see how much this doesnt work for cf

--- .travis-deploy/data.sh
+++ .travis-deploy/data.sh
@@ -3,14 +3,14 @@ DEPLOY_ENV=dev
 
 injectIt()
 {
-    S3_FOLDER=$S3_BUCKET-$DEPLOY_ENV
-    AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY aws s3 cp s3://"$S3_FOLDER"/$SERVICE_NAME/keys.json keys.json
-    for i in $(jq -r '.keys[] | "\(.key),\(.expires),\(.service)"' keys.json); do
-        IFS=','
-        read -ra SPL <<< "$i"
-        AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY aws dynamodb put-item --table-name $TABLE_NAME --item "{\"authKey\":{\"S\":\"${SPL[0]}\"},\"expires\":{\"N\":\"${SPL[1]}\"},\"service\":{\"S\":\"${SPL[2]}\"}}" --region $AWS_REGION
-        IFS=' '
-    done
+#    S3_FOLDER=$S3_BUCKET-$DEPLOY_ENV
+#    AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY aws s3 cp s3://"$S3_FOLDER"/$SERVICE_NAME/data.json data.json
+#    for i in $(jq -r '.keys[] | "\(.key),\(.expires),\(.service)"' keys.json); do
+#        IFS=','
+#        read -ra SPL <<< "$i"
+#        AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY aws dynamodb put-item --table-name $TABLE_NAME --item "{\"authKey\":{\"S\":\"${SPL[0]}\"},\"expires\":{\"N\":\"${SPL[1]}\"},\"service\":{\"S\":\"${SPL[2]}\"}}" --region $AWS_REGION
+#        IFS=' '
+#    done
 }
 
 
